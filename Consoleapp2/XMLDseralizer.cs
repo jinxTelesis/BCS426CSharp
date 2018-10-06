2bffc937568c19835cf4987aa1a7c30015032ac8
Dre <31423756+jinxTelesis@users.noreply.github.com>
2018-10-06 12:35:01 -0400

    Update XMLDseralizer.cs

--- Consoleapp2/XMLDseralizer.cs
+++ Consoleapp2/XMLDseralizer.cs
@@ -3,24 +3,59 @@ using hwk2Library_Andre_lussier;
 using System.Runtime.Serialization; // dll for Json
 using System.IO;
 
+//*****************************************
+// File XMLDSeralizer 
+//
+// Purpose: this deseralizes 3 different types to XML
+// the type is determined at run time via get type
+// but ive written 1000+ hours of python dyniamic typed code
+// and this seems to make sense to me 
+// hope to rewrite this for proper c# reflection standards once i know them
+//
+// Written By: Andre Lussier
+// 
+// Compiler: Visual Studios 2017 
+//
+//*****************************************
+
 
 namespace MyXMLDeSerNamespace {
 
-    public class JsonDseralizer
+    public class XMLDseralizer
     {
+
+        #region XMLDseralizer variables
         private String filename;
         private FileStream reader;
         //private DataContractJsonSerializer ser;
-        private Currency curP = new Currency(); // these are for the type comparison, think this should be reflections? 
-        private Language langP = new Language(); // but I don't know about that yet, just coded a lot of dynamic type checking
-        private Country countryP = new Country(); // python so this felt right
+        private Currency curP; // = new Currency(); // these are for the type comparison, think this should be reflections? 
+        private Language langP; // = new Language(); // but I don't know about that yet, just coded a lot of dynamic type checking
+        private Country countryP; // = new Country(); // python so this felt right
+        #endregion end of XMLDserilizer variables
 
-
-
-        public JsonDseralizer()
+        public XMLDseralizer()
         {
 
+            this.curP = new Currency();
+            this.langP = new Language();
+            this.countryP = new Country();
+
         }
+
+        /// <summary>
+        /// control statement to form better decoupled code
+        /// 
+        /// </summary>
+        /// <param name="filename">takes the filename location the user passes in </param> 
+        /// <param name="o"> takes the object type determined by constrol states
+        /// will be either Currency, Language or Country
+        /// 
+        /// if(o.GetType()== curP.GetType()) makes the runtime determination for late
+        /// 
+        /// </param>
+        /// 
+
+        #region XMLDserilizer methods
 
         public void setdeserilizer(String filename, Object o)
         {
@@ -60,8 +95,9 @@ namespace MyXMLDeSerNamespace {
 
             }
 
-
         }
+
+        #endregion end of XMLDserlizter methods 
 
     }
 }
